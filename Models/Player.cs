181f62f044bc9ff0443e3c9cf13c9e08fb857acd
PRAMANIK <850058666@genpact.com>
2022-02-25 17:58:27 +0530

    Initial commit

---
+++ Models/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppGitCheck.Models
+{
+    public class Player
+    {
+        public int PId { get; set; }
+        public string PName { get; set; }
+        public string PTeam { get; set; }
+    }
+}
