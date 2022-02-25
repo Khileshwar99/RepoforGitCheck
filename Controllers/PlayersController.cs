f66e9c49daa881def1daaa4ba3d586e1241e3f14
PRAMANIK <850058666@genpact.com>
2022-02-25 18:06:40 +0530

    After adding controller

---
+++ Controllers/PlayersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppGitCheck.Models;
+
+namespace WebAppGitCheck.Controllers
+{
+    public class PlayersController : Controller
+    {
+        // GET: Players
+        public ActionResult Index()
+        {
+            List<Player> playerList = new List<Player>
+            { 
+                new Player{PId=1,PName="MSD",PTeam="CSK"},
+                new Player{PId=2,PName="Virat",PTeam="RCB"},
+            };
+            return View(playerList);
+        }
+    }
+}
